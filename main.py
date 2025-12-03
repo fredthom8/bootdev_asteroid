b3c871644318e93a220b91a86099ab3443ddc559
fredthom8 <thomassin.frederic1@gmail.com>
2025-12-02 20:18:04 -0500

    Ajout du champ d'asteroides

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ import pygame
 from constants import SCREEN_WIDTH, SCREEN_HEIGHT
 from logger import log_state
 from player import Player
+from asteroid import Asteroid
+from asteroidfield import AsteroidField
 
 
 def main():
@@ -10,8 +12,18 @@ def main():
     clock = pygame.time.Clock()
     dt = 0
 
-    # Init the player
+    # Init the groups
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
+
+    AsteroidField.containers = (updatable)
+    Asteroid.containers = (updatable, drawable, asteroids)
+    Player.containers = (updatable, drawable)
+
+    # Init the player and field
     player = Player(SCREEN_WIDTH/2, SCREEN_HEIGHT/2)
+    asteroidfield = AsteroidField()
 
     # Game loop
     while True:
@@ -20,10 +32,12 @@ def main():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-        player.update(dt)
-
+        updatable.update(dt)
         screen.fill("black")
-        player.draw(screen)
+
+        for t in drawable:
+            t.draw(screen)
+       
         pygame.display.flip()
 
         # 60 FPS
